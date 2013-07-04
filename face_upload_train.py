678a6c487a121cc675f21a73a10a971c1b8dccdd
dc401 <dchow@xtecsystems.com>
2013-07-04 07:53:51 -0500

    Update face_upload_train.py
    Updated to GPL v3

--- face_upload_train.py
+++ face_upload_train.py
@@ -4,7 +4,7 @@
 LICENSING:
 Copyright (C)  2013  DENNIS CHOW dchowATxtecsystems.com.
 Permission is granted to copy, distribute and/or modify this document
-under the terms of the GNU Free Documentation License, Version 1.3
+under the terms of the GNU Free Documentation License, Version 3
 or any later version published by the Free Software Foundation;
 with no Invariant Sections, no Front-Cover Texts, and no Back-Cover Texts.
 A copy of the license is included in the section entitled "GNU
